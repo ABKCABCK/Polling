421b4472bb84d4ea5d42ad7d99f63997b66c9dd9
kkkk <chaokobe@gmail.com>
2020-12-16 17:30:27 +0800

    add polling contract and raw test cases

---
+++ contracts/Polling.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Polling {
+    struct Poll {
+        string topic;
+        string description;
+        uint256 expiredBlock;
+        address sponsor;
+        address[] voters;
+    }
+    // track poll by poll id
+    mapping(bytes32 => Poll) public polls;
+    // track poll id for sponsors
+    mapping(address => bytes32[]) public pollIdOfSponsor;
+    mapping(address => mapping(bytes32 => bool)) private sponsorRaised;
+    // track poll id being polled by voter
+    mapping(address => bytes32[]) public pollIdOfVoter;
+    mapping(address => mapping(bytes32 => bool)) private voterPolled;
+
+    uint256 public totalPollsCount;
+    bytes32[] public pollList;
+
+    constructor() public {
+        totalPollsCount = 0;
+    }
+
+    function sponsorCreatePoll(
+        string calldata _topic,
+        string calldata _description,
+        uint256 _expiredBlock
+    ) external returns (bool) {
+        require(_expiredBlock > block.number, "Invalid expired block");
+
+        address _sponsor = msg.sender;
+
+        uint256 _totalPollsCount = totalPollsCount;
+        totalPollsCount += 1;
+
+        bytes32 _pollId = keccak256(
+            abi.encodePacked(_sponsor, _totalPollsCount, _topic, _expiredBlock)
+        );
+
+        require(!sponsorRaised[_sponsor][_pollId], "Poll is existed");
+
+        Poll memory _poll;
+        _poll.topic = _topic;
+        _poll.description = _description;
+        _poll.expiredBlock = _expiredBlock;
+        _poll.sponsor = _sponsor;
+
+        polls[_pollId] = _poll;
+        pollIdOfSponsor[_sponsor].push(_pollId);
+        sponsorRaised[_sponsor][_pollId] = true;
+
+        pollList.push(_pollId);
+
+        return true;
+    }
+
+    function voterPolls(bytes32 _pollId) external returns (bool) {
+        require(polls[_pollId].sponsor != address(0), "Poll isn't existed");
+
+        (, bool _expired) = getPollStatus(_pollId);
+        require(!_expired, "Poll is expired");
+
+        address _voter = msg.sender;
+        require(!voterPolled[_voter][_pollId], "You've polled for this");
+
+        Poll storage _poll = polls[_pollId];
+
+        _poll.voters.push(_voter);
+        pollIdOfVoter[_voter].push(_pollId);
+        voterPolled[_voter][_pollId] = true;
+        return true;
+    }
+
+    function getPollInformation(bytes32 _pollId)
+        public
+        view
+        returns (
+            string memory _topic,
+            string memory _description,
+            address _sponsor,
+            address[] memory _voters,
+            uint256 _blockLeft,
+            bool _expired
+        )
+    {
+        Poll memory poll = polls[_pollId];
+
+        _topic = poll.topic;
+        _description = poll.description;
+        _sponsor = poll.sponsor;
+        _voters = poll.voters;
+        (_blockLeft, _expired) = getPollStatus(_pollId);
+    }
+
+    function getPollStatus(bytes32 _pollId)
+        internal
+        view
+        returns (uint256 _blockLeft, bool _expired)
+    {
+        int256 blockLeft = int256(block.number - polls[_pollId].expiredBlock);
+        _blockLeft = blockLeft <= 0 ? 0 : uint256(blockLeft);
+        _expired = block.number >= polls[_pollId].expiredBlock;
+    }
+
+    function getPollList() public view returns (bytes32[] memory) {
+        return pollList;
+    }
+
+    function getSponsorsPollList() public view returns (bytes32[] memory) {
+        return pollIdOfSponsor[msg.sender];
+    }
+
+    function getVotersPollList() public view returns (bytes32[] memory) {
+        return pollIdOfVoter[msg.sender];
+    }
+}
---
+++ test/poll.js
@@ -0,0 +1,220 @@
+const Polling = artifacts.require("Polling");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+
+const truffleAssert = require('truffle-assertions');
+
+contract("Polling", function (accounts) {
+  const TEST_TOPIC = "TEST_TOPIC";
+  const TEST_DESCRIPTION = "TEST_DESCRIPTION";
+
+  let testSponsor, testVoter, testExpiredBlock;
+  let pollContractInstance;
+  let finalPollList;
+
+  before(async () => {
+    testSponsor = accounts[0];
+    testVoter = accounts[1];
+    testExpiredBlock = 999999999;
+
+    pollContractInstance = await Polling.deployed();
+    console.log({
+      contractAddress: pollContractInstance.address,
+      testSponsor,
+      testVoter,
+      testExpiredBlock,
+    })
+  })
+
+  it("should successfully create a poll", async function () {
+
+    let pollList, pollIdOfSponsor;
+
+    pollList = await pollContractInstance.getPollList();
+    pollIdOfSponsor = await pollContractInstance.getSponsorsPollList({ from: testSponsor });
+    assert.strictEqual(pollList.length, 0, "Polling Contract: invalid poll list length before first polling");
+    assert.strictEqual(pollIdOfSponsor.length, 0, "Polling Contract: invalid poll length of sponsor before first polling");
+
+    await pollContractInstance.sponsorCreatePoll(
+      TEST_TOPIC,
+      TEST_DESCRIPTION,
+      testExpiredBlock,
+      { from: testSponsor }
+    );
+
+    pollList = await pollContractInstance.getPollList();
+    pollIdOfSponsor = await pollContractInstance.getSponsorsPollList({ from: testSponsor });
+    assert.strictEqual(pollList.length, 1, "Polling Contract: invalid poll list length after first polling");
+    assert.strictEqual(pollIdOfSponsor.length, 1, "Polling Contract: invalid poll length of sponsor after first polling");
+
+    await pollContractInstance.sponsorCreatePoll(
+      TEST_TOPIC,
+      TEST_DESCRIPTION,
+      testExpiredBlock,
+      { from: testSponsor }
+    );
+
+    pollList = await pollContractInstance.getPollList();
+    pollIdOfSponsor = await pollContractInstance.getSponsorsPollList({ from: testSponsor });
+    assert.strictEqual(pollList.length, 2, "Polling Contract: invalid poll list length after second polling");
+    assert.strictEqual(pollIdOfSponsor.length, 2, "Polling Contract: invalid poll length of sponsor after second polling");
+
+    await pollContractInstance.sponsorCreatePoll(
+      TEST_TOPIC,
+      TEST_DESCRIPTION,
+      testExpiredBlock,
+      { from: accounts[3] }
+    );
+
+    pollList = await pollContractInstance.getPollList();
+    pollIdOfSponsor = await pollContractInstance.getSponsorsPollList({ from: accounts[3] });
+    assert.strictEqual(pollList.length, 3, "Polling Contract: invalid poll list length after third polling");
+    assert.strictEqual(pollIdOfSponsor.length, 1, "Polling Contract: invalid poll length of sponsor after third polling");
+
+    finalPollList = pollList;
+    return true;
+  });
+
+  it("should fail to create a poll", async function () {
+    truffleAssert.fails(
+      pollContractInstance.sponsorCreatePoll(
+        TEST_TOPIC,
+        TEST_DESCRIPTION,
+        1,
+        { from: testSponsor }
+      ),
+      truffleAssert.ErrorType.REVERT,
+      "Invalid expired block",
+      "Polling Contract: should provide correct expired block number",
+    );
+
+    let pollList = await pollContractInstance.getPollList();
+    return assert.strictEqual(pollList.length, 3, "Polling Contract: invalid poll list length after failing to create a poll");
+  });
+
+  it("should successfully vote to a poll", async function () {
+
+    let pollId = finalPollList[0];
+
+    let status, pollIdOfVoter;
+
+    status = await pollContractInstance.polls(pollId);
+    pollIdOfVoter = await pollContractInstance.getVotersPollList({ from: testVoter });
+    assert.strictEqual(status._voters, undefined, "Polling Contract: invalid voter length of poll before first voting at 0");
+    assert.strictEqual(pollIdOfVoter.length, 0, "Polling Contract: invalid poll length of voter before first voting at 0");
+
+    await pollContractInstance.voterPolls(
+      pollId,
+      { from: testVoter }
+    );
+    status = await pollContractInstance.getPollInformation(pollId);
+    pollIdOfVoter = await pollContractInstance.getVotersPollList({ from: testVoter });
+    assert.strictEqual(status._voters.length, 1, "Polling Contract: invalid voter length of poll after first voting at 0");
+    assert.strictEqual(pollIdOfVoter.length, 1, "Polling Contract: invalid poll length of voter after first voting at 0");
+
+    await pollContractInstance.voterPolls(
+      pollId,
+      { from: accounts[2] }
+    );
+    status = await pollContractInstance.getPollInformation(pollId);
+    pollIdOfVoter = await pollContractInstance.getVotersPollList({ from: accounts[2] });
+    assert.strictEqual(status._voters.length, 2, "Polling Contract: invalid voter length of poll after second voting at 0");
+    assert.strictEqual(pollIdOfVoter.length, 1, "Polling Contract: invalid poll length of voter after second voting at 0");
+
+    pollId = finalPollList[1];
+
+    await pollContractInstance.voterPolls(
+      pollId,
+      { from: testVoter }
+    );
+    status = await pollContractInstance.getPollInformation(pollId);
+    pollIdOfVoter = await pollContractInstance.getVotersPollList({ from: testVoter });
+    assert.strictEqual(status._voters.length, 1, "Polling Contract: invalid voter length of poll after first voting at 1");
+    assert.strictEqual(pollIdOfVoter.length, 2, "Polling Contract: invalid poll length of voter after first voting at 1");
+
+    pollId = finalPollList[2];
+
+    await pollContractInstance.voterPolls(
+      pollId,
+      { from: testVoter }
+    );
+    status = await pollContractInstance.getPollInformation(pollId);
+    pollIdOfVoter = await pollContractInstance.getVotersPollList({ from: testVoter });
+    assert.strictEqual(status._voters.length, 1, "Polling Contract: invalid voter length of poll after first voting at 1");
+    assert.strictEqual(pollIdOfVoter.length, 3, "Polling Contract: invalid poll length of voter after first voting at 1");
+
+    return true;
+  });
+
+  it("should fail to vote again", async function () {
+    truffleAssert.fails(
+      pollContractInstance.voterPolls(
+        finalPollList[0],
+        { from: testVoter }
+      ),
+      truffleAssert.ErrorType.REVERT,
+      "You've polled for this",
+      "Polling Contract: should not be voted again at 0",
+    );
+
+    truffleAssert.fails(
+      pollContractInstance.voterPolls(
+        finalPollList[1],
+        { from: testVoter }
+      ),
+      truffleAssert.ErrorType.REVERT,
+      "You've polled for this",
+      "Polling Contract: should not be voted again at 1",
+    );
+
+    truffleAssert.fails(
+      pollContractInstance.voterPolls(
+        finalPollList[2],
+        { from: testVoter }
+      ),
+      truffleAssert.ErrorType.REVERT,
+      "You've polled for this",
+      "Polling Contract: should not be voted again at 2",
+    );
+
+    let status;
+
+    status = await pollContractInstance.getPollInformation(finalPollList[0]);
+    assert.strictEqual(status._voters.length, 2, "Polling Contract: invalid voter length at 0");
+
+    status = await pollContractInstance.getPollInformation(finalPollList[1]);
+    assert.strictEqual(status._voters.length, 1, "Polling Contract: invalid voter length at 1");
+
+    status = await pollContractInstance.getPollInformation(finalPollList[2]);
+    assert.strictEqual(status._voters.length, 1, "Polling Contract: invalid voter length at 2");
+
+  });
+
+  it("should fail to vote a inexisted poll", async function () {
+    truffleAssert.fails(
+      pollContractInstance.voterPolls(
+        "0xfoobar",
+        { from: testVoter }
+      ),
+      truffleAssert.ErrorType.REVERT,
+      "Poll isn't existed",
+      "Polling Contract: inexisted poll should not be voted",
+    );
+
+    truffleAssert.fails(
+      pollContractInstance.voterPolls(
+        undefined,
+        { from: testVoter }
+      ),
+      truffleAssert.ErrorType.REVERT,
+      "Poll isn't existed",
+      "Polling Contract: inexisted poll should not be voted",
+    );
+
+  });
+
+});
