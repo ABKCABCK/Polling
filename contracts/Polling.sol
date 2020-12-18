fc08ed4196510c1b838269ca69563f94d4ca0e14
kkkk <chaokobe@gmail.com>
2020-12-18 17:25:26 +0800

    add choice array to poll struct

--- contracts/Polling.sol
+++ contracts/Polling.sol
@@ -4,10 +4,16 @@ pragma solidity >=0.4.22 <0.8.0;
 contract Polling {
     struct Poll {
         string topic;
-        string description;
-        uint256 expiredBlock;
         address sponsor;
+        string description;
+        uint[] options;
+        uint256 expiredBlock;
         address[] voters;
+    }
+
+    struct Answer {
+        bytes32 pollId;
+        uint choice;
     }
     // track poll by poll id
     mapping(bytes32 => Poll) public polls;
@@ -15,7 +21,8 @@ contract Polling {
     mapping(address => bytes32[]) public pollIdOfSponsor;
     mapping(address => mapping(bytes32 => bool)) private sponsorRaised;
     // track poll id being polled by voter
-    mapping(address => bytes32[]) public pollIdOfVoter;
+    mapping(address => Answer[]) public answerOfVoter;
+    // mapping(address => bytes32[]) public pollIdOfVoter;
     mapping(address => mapping(bytes32 => bool)) private voterPolled;
 
     uint256 public totalPollsCount;
@@ -28,6 +35,7 @@ contract Polling {
     function sponsorCreatePoll(
         string calldata _topic,
         string calldata _description,
+        uint[] calldata _options,
         uint256 _expiredBlock
     ) external returns (bool) {
         require(_expiredBlock > block.number, "Invalid expired block");
@@ -45,9 +53,10 @@ contract Polling {
 
         Poll memory _poll;
         _poll.topic = _topic;
-        _poll.description = _description;
-        _poll.expiredBlock = _expiredBlock;
         _poll.sponsor = _sponsor;
+        _poll.description = _description;
+        _poll.options = _options;
+        _poll.expiredBlock = _expiredBlock;
 
         polls[_pollId] = _poll;
         pollIdOfSponsor[_sponsor].push(_pollId);
@@ -58,7 +67,7 @@ contract Polling {
         return true;
     }
 
-    function voterPolls(bytes32 _pollId) external returns (bool) {
+    function voterPolls(bytes32 _pollId, uint _choice) external returns (bool) {
         require(polls[_pollId].sponsor != address(0), "Poll isn't existed");
 
         (, bool _expired) = getPollStatus(_pollId);
@@ -67,11 +76,15 @@ contract Polling {
         address _voter = msg.sender;
         require(!voterPolled[_voter][_pollId], "You've polled for this");
 
-        Poll storage _poll = polls[_pollId];
-
-        _poll.voters.push(_voter);
-        pollIdOfVoter[_voter].push(_pollId);
+        polls[_pollId].voters.push(_voter);
+        // pollIdOfVoter[_voter].push(_pollId);
         voterPolled[_voter][_pollId] = true;
+
+        Answer memory answer = Answer({
+            pollId: _pollId,
+            choice: _choice
+        });
+        answerOfVoter[_voter].push(answer);
         return true;
     }
 
@@ -82,6 +95,7 @@ contract Polling {
             string memory _topic,
             string memory _description,
             address _sponsor,
+            uint[] memory _options,
             address[] memory _voters,
             uint256 _blockLeft,
             bool _expired
@@ -92,6 +106,7 @@ contract Polling {
         _topic = poll.topic;
         _description = poll.description;
         _sponsor = poll.sponsor;
+        _options = poll.options;
         _voters = poll.voters;
         (_blockLeft, _expired) = getPollStatus(_pollId);
     }
@@ -114,9 +129,9 @@ contract Polling {
         return pollIdOfSponsor[msg.sender];
     }
 
-    function getVotersPollList() public view returns (bytes32[] memory) {
-        return pollIdOfVoter[msg.sender];
-    }
+    // function getVotersPollList() public view returns (bytes32[] memory) {
+    //     return pollIdOfVoter[msg.sender];
+    // }
 
     function isVoted(bytes32 _pollId) public view returns (bool) {
         return voterPolled[msg.sender][_pollId];
